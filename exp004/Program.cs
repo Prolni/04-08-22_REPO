46c41e307df2613c2147333bab43d56616805a6a
OlegPriemyshev <prolni@mail.ru>
2022-08-04 13:46:10 +0300

    добавили exp004+разные действия

--- exp004/Program.cs
+++ exp004/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int A = 12;
+int B = 5;
+Console.WriteLine(A + B);
+Console.WriteLine(A / B);
+double C = 16;
+double D = 5;
+Console.WriteLine(C + D);
+Console.WriteLine(C / D);
