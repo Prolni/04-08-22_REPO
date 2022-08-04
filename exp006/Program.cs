49409d828810ff8a3f7d7bf617ec97fe12f31217
OlegPriemyshev <prolni@mail.ru>
2022-08-04 14:56:03 +0300

    добавили exp006 + поиск и печать тяжелой гири

---
+++ exp006/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Гири! Ищем самую тяжелую:");
+int a = 4;
+int b = 2;
+int c = 9;
+int d = 1;
+int e = 5;
+int f = 7;
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+if(f > max) max = f;
+Console.Write("Самая тяжелая гиря, кг: ");
+Console.WriteLine(max);
