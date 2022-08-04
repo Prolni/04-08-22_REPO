9a182902d60215f2619acad03e151a4dd3aa797d
OlegPriemyshev <prolni@mail.ru>
2022-08-04 14:34:42 +0300

    добавили exp005 + проверка имен с условием if else

---
+++ exp005/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Определяем по верности введения имени");
+Console.Write("Введите Ваше Имя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша")
+{
+  Console.WriteLine("Здравствуйте, Маша!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(userName);
+}
