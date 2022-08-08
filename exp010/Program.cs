1758b89cba75ba7140fb99e7c7f213e8e87ce7c3
OlegPriemyshev <prolni@mail.ru>
2022-08-08 18:48:34 +0300

    добавили exp010 + создали поиск позиции искомого значения из массива

---
+++ exp010/Program.cs
@@ -0,0 +1,19 @@
+﻿// поиск индекса заданного числа
+int [] array =  {21, 42, 3500, 48, 65, 106, 67, 908, 908};
+
+int n = array.Length;
+
+int find = 908;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //для окончания поиска, чтобы не вывоились все позиции одинаковых чисел
+    }
+          
+    index++; //  index = index + 1
+
+}
