83724d147a61b659354e220eacbe2b40b11ff019
OlegPriemyshev <prolni@mail.ru>
2022-08-08 19:34:18 +0300

    добавили exp011 + пока не работает!!!

---
+++ exp011/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index  = 0;
+    while (index < lenght)
+    {
+       collection[index] = new Random().Next(1, 10);
+
+       index++;
+
+    }
+}
+
+void PrintArray(int[] collect)
+{
+    int position = collect.Length;
+    int count  = 0;
+    while (count < position)
+    {
+       Console.WriteLine(collect[position]);
+       count++;
+
+    }
+}
+
+int []array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+
+
+
+
+
