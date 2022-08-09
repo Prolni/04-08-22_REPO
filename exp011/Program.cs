0e954736db2f6989bab3471e2023a6deeb1e0b9a
OlegPriemyshev <prolni@mail.ru>
2022-08-09 10:43:50 +0300

    добавили exp011

--- exp011/Program.cs
+++ exp011/Program.cs
@@ -6,7 +6,7 @@ void FillArray(int[] collection)
     while (index < lenght)
     {
        collection[index] = new Random().Next(1, 10);
-
+      //Console.WriteLine(collection[index]);
        index++;
 
     }
@@ -16,11 +16,12 @@ void PrintArray(int[] collect)
 {
     int position = collect.Length;
     int count  = 0;
+
     while (count < position)
     {
-       Console.WriteLine(collect[position]);
+       Console.WriteLine(collect[count]);
        count++;
-
+       // Console.WriteLine(count);
     }
 }
 
