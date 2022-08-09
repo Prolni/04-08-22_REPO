53de2d7b77dd2fb94ede45d2ac56868e24978adc
OlegPriemyshev <prolni@mail.ru>
2022-08-09 14:44:08 +0300

    дз семинар1+задача 8

--- seminar004/Program.cs
+++ seminar004/Program.cs
@@ -4,3 +4,30 @@
 //8 -> 2, 4, 6, 8
 
 Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num <= 0)
+Console.WriteLine($"Введенное число: {num}, меньше или равно 0, введите число более 0!");
+
+int control = num % 2;
+if (control == 0)
+{
+
+while( num > 0)
+{
+Console.WriteLine(num);
+num = num -2;
+}
+}
+else
+{while( num > 1)
+{
+Console.WriteLine(num-1);
+num = num -2;
+}
+}
+
+// Итог: принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
