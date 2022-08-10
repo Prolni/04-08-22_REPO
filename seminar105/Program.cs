ce8f4a3104398fbd4886fc575013028bda6e6229
OlegPriemyshev <prolni@mail.ru>
2022-08-10 14:32:10 +0300

    дз семинар2+задача 13

--- seminar105/Program.cs
+++ seminar105/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: 
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num < 100) Console.WriteLine($"В введенном числе: {num} третья цифра отсутствует, введите новое число");
+
+int element = num % 10;
+
+while (num > 999)
+{
+    num = (num - element) / 10
+}
+
+Console.WriteLine($"В введенном числе третья цифра: {element}");
