7b2b9d9a847b94e57859c9a4b9c9ba93b8007701
OlegPriemyshev <prolni@mail.ru>
2022-08-10 15:53:53 +0300

    дз семинар2+задача 10

--- seminar106/Program.cs
+++ seminar106/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//82 -> 8
+//918 -> 1
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 || num > 999) 
+{
+Console.WriteLine($"Введенное число: {num} не является положительным трехзначным числом, введите новое число");
+}
+else
+{
+int element = num / 10 % 10; 
+Console.WriteLine($"В введенном числе вторая цифра: {element}");
+}
