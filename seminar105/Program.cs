37554e3df54d98c28ab2e20bcdc92932b57bd9bd
OlegPriemyshev <prolni@mail.ru>
2022-08-10 14:57:47 +0300

    дз семинар2+задача 13

--- seminar105/Program.cs
+++ seminar105/Program.cs
@@ -8,13 +8,17 @@ Console.Clear();
 Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 
-if(num < 100) Console.WriteLine($"В введенном числе: {num} третья цифра отсутствует, введите новое число");
+if(num < 100) Console.WriteLine($"В введенном числе: {num} третья цифра отсутствует или число отрицательное, введите новое число");
 
+else
+{
+    
 int element = num % 10;
 
 while (num > 999)
 {
-    num = (num - element) / 10
-}
-
+    num = (num - element) / 10;
+    element = num % 10;
+   }
 Console.WriteLine($"В введенном числе третья цифра: {element}");
+}
