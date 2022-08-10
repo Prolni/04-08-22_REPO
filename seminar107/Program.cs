2d0106e69a16ff2bab27583b4a4a7f7c826db60a
OlegPriemyshev <prolni@mail.ru>
2022-08-10 16:08:20 +0300

    дз семинар2+задача 15

--- seminar107/Program.cs
+++ seminar107/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 15: 
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+Console.Clear();
+
+Console.Write("Введите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 1 || num > 7) 
+Console.WriteLine($"Введенное число: {num} не является задаваемым числом от 1 до 7, введите новое число");
+
+else if (num == 6 || num == 7)
+Console.WriteLine($"{num} день недели - Выходной");
+else 
+Console.WriteLine($"{num} день недели - НЕ Выходной ((");
