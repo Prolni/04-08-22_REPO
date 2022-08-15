a993007b0565a4b37bef413b877b9800df9cd683
OlegPriemyshev <prolni@mail.ru>
2022-08-15 18:38:03 +0300

    дз семинар3 seminar209

---
+++ seminar209/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 23
+Напишите программу, которая принимает
+ на вход число (N) и выдаёт таблицу 
+кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int n = 1; n <= N; n++)
+{
+    int cube_n = n * n * n;
+    if (n == N)
+    {
+        Console.Write(cube_n);
+    }
+    else
+    {
+        Console.Write(cube_n + ", ");
+    }
+ }
