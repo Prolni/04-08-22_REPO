9adc5bbedc750b1e5ce5a8785c8ddc8f9d97daeb
OlegPriemyshev <prolni@mail.ru>
2022-09-05 19:43:33 +0300

    дз семинар9 seminar64

---
+++ seminar_9_64/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от M до N от большего к меньшему.
+M = 1; N = 5. -> ""5, 4, 3, 2, 1""
+M = 4; N = 8. -> ""8, 7, 6, 5, 4""
+*/
+using static System.Console;
+Clear();
+
+WriteLine("Задайте два числа через Enter: ");
+
+int n = int.Parse(ReadLine());
+int m = int.Parse(ReadLine());
+
+void Natural(int m, int n)
+{
+       if (m <= n) 
+    {
+        Write($"{n} ");
+        if (m == n) return;
+        Natural(n - 1, m);
+    }
+    else if (m >= n)
+    {
+        Write($"{m} ");
+        if (m == n) return;
+        Natural(m - 1, n);        
+    }
+}
+Natural(n, m);
