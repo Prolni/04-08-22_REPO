bac793cca43cbb8507361a03f7cadf7baf4efb89
OlegPriemyshev <prolni@mail.ru>
2022-09-06 16:48:16 +0300

    дз семинар9 seminar66

---
+++ seminar_9_66/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 66: 
+Задайте значения M и N.
+Напишите программу, которая найдёт сумму натуральных 
+элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+using static System.Console;
+Clear();
+
+
+int m = InputNumbers("Ведите первое число диапазона: ");
+int n = InputNumbers("Ведите второе число диапазона: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Write($"Сумма чисел в заданном диапазоне: {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
